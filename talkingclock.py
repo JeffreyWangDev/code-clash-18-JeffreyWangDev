60a1c2c4f4785da2d1fa4e039d100d4bf90c204b
Jeffrey Wang <66625372+JeffreyWangDev@users.noreply.github.com>
2023-08-04 09:23:02 -0400

    Update talkingclock.py

--- talkingclock.py
+++ talkingclock.py
@@ -24,24 +24,9 @@ Input: 23:59  Output: It's eleven fifty nine pm
 Input: 12:05  Output: It's twelve oh five pm
 """
 
-
-hours = {1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve",0:"twelve"}
-minutes = {1:"oh one",2:"oh two",3:"oh three",4:"oh four",5:"oh five",10:"ten",11:"eleven",13:"thirteen",14:"fourteen",19:"nineteen",20:"twenty",30:"thirty",29:"twenty nine",39:"thirty nine",49:"forty nine",59:"fifty nine"}
-    # This will convert military hours to regular hours, and determine AM vs PM
 class Solution:    
     def ClockTalker(self, input_time):
-        a = ""
-        if int(input_time.split(":")[0]) < 12:
-            a="am"
-            hour = int(input_time.split(":")[0])
-        else:
-            a="pm"
-            hour = int(input_time.split(":")[0]) - 12
-        time = ""
-        time += hours[hour]
-        if int(input_time.split(":")[1]) in minutes:
-            time += " " + minutes[int(input_time.split(":")[1])]
-        return "It's " +time +" "+ a
+                return "It's " +{1:"one", 2:"two", 3:"three", 4:"four", 5:"five", 6:"six",7:"seven", 8:"eight", 9:"nine", 10:"ten", 11:"eleven", 12:"twelve", 0:"twelve"}[int(input_time.split(":")[0]) if int(input_time.split(":")[0]) < 12 else int(input_time.split(":")[0]) - 12]+ " " + {1:"oh one",2:"oh two",3:"oh three",4:"oh four",5:"oh five",10:"ten",11:"eleven",13:"thirteen",14:"fourteen",19:"nineteen",20:"twenty",30:"thirty",29:"twenty nine",39:"thirty nine",49:"forty nine",59:"fifty nine"}[int(input_time.split(":")[1])] +" "+ ("am" if int(input_time.split(":")[0])>12 else "pm")
 def main():
      str1=input()
      tc1= Solution()
